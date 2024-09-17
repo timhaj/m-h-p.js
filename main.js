31f30bfae26373b0017706a9f71ec37f7055ebac
Tim Hajdinjak <69644152+timhaj@users.noreply.github.com>
2024-09-17 21:28:51 +0200

    Added image support

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 
-function parser(text){
+function parser(text) {
     let re, x;
 
+    re = /\!\[(.*)\]\((.*)\)/gm;
+    x = text.replace(re, '<img src="$2" alt="$1" />');
+
+    //blockquote
     re = /\>\s(.*)/gm;
-    x = text.replace(re, '<blockquote>$1</blockquote>');
+    x = x.replace(re, '<blockquote>$1</blockquote>');
 
-
+    //unordered list
     const unordered = x.replace(/(?:^|\n)([-+*] .+(?:\n[-+*] .+)*)/gm, (match) => {
         const listItems = match.trim().split('\n').map(item => {
             return item.replace(/^[-+*] /, '\n<li>') + '</li>';
@@ -14,6 +18,7 @@ function parser(text){
     });
     x = unordered;
 
+    //ordered list
     const ordered = x.replace(/(?:^|\n)([0-9]+[.] .+(?:\n[0-9]+[.] .+)*)/gm, (match) => {
         const listItems = match.trim().split('\n').map(item => {
             return item.replace(/^[0-9]+[.] /, '\n<li>') + '</li>';
@@ -22,19 +27,25 @@ function parser(text){
     });
     x = ordered;
 
+    //bold
     re = /\*\*(.*?)\*\*/gm;
     x = x.replace(re, '<strong>$1</strong>');
+    //heading
     const htmlText = x.replace(/^(#{1,6})\s(.*)/gm, (match, hashes, headingText) => {
         const headingLevel = hashes.length;
         return `<h${headingLevel}>${headingText}</h${headingLevel}>`;
-      });
-    x= htmlText;
+    });
+    x = htmlText;
+    //horizontal rule
     re = /(\*{3}|\-{3}|\_{3})/gm;
     x = x.replace(re, '<hr>');
+    //italic
     re = /\*(.*?)\*/gm;
     x = x.replace(re, '<em>$1</em>');
+    //link pt.1
     re = /\[(.*)\]\((.*)\)/gm;
     x = x.replace(re, '<a href="$2">$1</a>')
+    //link pt.2
     //re = /(http[s]?\:\/{2}[^\s]*)/gm; //ce slucajno ne damo v []() //ta tokenizacijo potem
     //x = x.replace(re, '<a href="$1">$1</a>')
     console.log(x);
@@ -42,9 +53,9 @@ function parser(text){
 }
 
 
-function mh_parse(){
+function mh_parse() {
     let nodes = document.getElementsByClassName('mh_parse');
-    for(let i = 0;i<nodes.length;i++){
+    for (let i = 0; i < nodes.length; i++) {
         nodes[i].innerHTML = parser(nodes[i].textContent);
     }
 }
